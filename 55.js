de4e0615be4951607e9065595b2080e2572fde6a
Guillaume <guillaume.chatriot@gmail.com>
2022-10-14 19:14:04 +0200

    Update 55.js

--- 55.js
+++ 55.js
@@ -4,11 +4,10 @@ const observer = new IntersectionObserver((entries) => {
   entries.forEach((item) => {
     let name = item.target.querySelector(
       ".c-product__name",
-      item.isIntersecting
+
     );
     let price = item.target.querySelector(
       ".c-price__value--current",
-      item.isIntersecting
     );
     if (item.isIntersecting === true)
       console.log(`Name:${name.innerText}, Price:${price.innerText}`);
